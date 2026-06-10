5af939fdfe37fef762915d2bbba698cffcce37e4
Sanjaya Ilham Kurnia <sanjayailham85@gmail.com>
2026-06-10 20:42:23 +0700

    perbaiki socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,8 +2,9 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 
-export const socket = io(SOCKET_URL, {
-  transports: ["websocket"],
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  transports: ["polling", "websocket"],
+  withCredentials: true,
 });
 
 socket.on("connect", () => {
