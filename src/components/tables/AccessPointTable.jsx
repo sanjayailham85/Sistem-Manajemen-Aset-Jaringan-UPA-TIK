6ae17fbcc609e7fe5b6733efb19d6c2a0ec4f260
Sanjaya Ilham Kurnia <sanjayailham85@gmail.com>
2026-06-10 17:01:15 +0700

    memperbaharui import accesspointmodal

--- src/components/tables/AccessPointTable.jsx
+++ src/components/tables/AccessPointTable.jsx
@@ -140,7 +140,7 @@ const AccessPointTable = () => {
             onClick={() => setShowImport(true)}
             className="bg-green-600 text-white px-4 py-2 rounded cursor-pointer"
           >
-            Import Data
+            Import Dataa
           </button>
         )} */}
 
